bfce40eb8166f3ede554059bcc4ce57138dc9db4
Sakshi Mittal <sakshimittal753@gmail.com>
2026-07-09 08:10:04 +0530

    Final

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -12,6 +12,7 @@ export const sendEmail = async ({ to, subject, html }: EmailOptions) => {
       host: 'smtp.gmail.com',
       port: 587,
       secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
